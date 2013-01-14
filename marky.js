efe571e1c8e175ec059c20790ff93cc7c5336254
Stefan Verhoeff <stefan_verhoeff@yahoo.com>
2013-01-14 23:12:56 +0100

    Homegrown map init + showing a POI on RND click

--- marky.js
+++ marky.js
@@ -1,24 +1,44 @@
 (function() {
-    console.log('woa!');
+    nokia.mh5.assetsPath = "http://api.maps.nokia.com/mobile/0.2.0/lib/";
+    nokia.mh5.appId = "_peU-uCkp-j8ovkzFGNU";
+    nokia.mh5.appCode = "gBoUkAMoxoqIWfxWA5DuMQ";
+
+    var map = new nokia.mh5.components.Map({
+        center: {
+            longitude: 13.3896145,
+            latitude: 52.5166648
+        },
+        zoom: 12,
+        schema: "normal.day",
+        tracking: true,
+        listeners: {
+            poiclick: function(e) {/*your listener*/},
+            mapmovestart: function() {/*your listener*/},
+            mapmoveend: function() {/*your listener*/},
+            mapclick: function(e) {/*your listener*/},
+            mapdblclick: function(e) {/*your listener*/},
+            maplongpress: function(e) {/*your listener*/},
+            maptrackingstart: function() {/*your listener*/},
+            maptrackingend: function() {/*your listener*/},
+            mapzoomchange: function(e) {/*your listener*/}
+        }
+    });
+
+    document.getElementById("app_location").appendChild(map.root);
 
     $('#rnd').click(function () {
         $( "#popupAmazing" ).popup("open");
 
         setTimeout(function () {
+            map.createPoi(
+                    "blue-ball.png", {
+                    longitude: 13.3896145,
+                    latitude: 52.5166648,
+                    name: "Nokia Office"
+            });
+
             $( "#popupAmazing" ).popup( "close" )
-        }, 2000);
+        }, 1300);
     });
 
-    nokia.mh5.assetsPath = "http://api.maps.nokia.com/mobile/0.2.0/lib/";
-    nokia.mh5.app.embed({
-        domNode: "#app_location",
-        appId: "_peU-uCkp-j8ovkzFGNU",
-        appCode: "gBoUkAMoxoqIWfxWA5DuMQ",
-        configuration: {
-            map: {
-            },
-            search: null,
-            details: null
-        }
-    });
 })();
