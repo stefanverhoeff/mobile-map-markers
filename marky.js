9dc308d3ce35964791699cf9dd894f3a7a79db67
Stefan Verhoeff <stefan_verhoeff@yahoo.com>
2013-01-14 21:20:57 +0100

    It's a fullscreen maps experience

---
+++ marky.js
@@ -0,0 +1,10 @@
+(function() {
+    console.log('woa!');
+
+    nokia.mh5.assetsPath = "http://api.maps.nokia.com/mobile/0.2.0/lib/";
+    nokia.mh5.app.embed({
+        domNode: "#app_location",
+        appId: "_peU-uCkp-j8ovkzFGNU",
+        appCode: "gBoUkAMoxoqIWfxWA5DuMQ"
+    });
+})();
