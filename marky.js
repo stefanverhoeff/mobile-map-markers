28f826453bd8b3823b702e0f5d6a8e8972f8100c
Stefan Verhoeff <stefan_verhoeff@yahoo.com>
2013-01-14 21:58:03 +0100

    Added jQuery

--- marky.js
+++ marky.js
@@ -1,6 +1,10 @@
 (function() {
     console.log('woa!');
 
+    $('#rnd').click(function () {
+        alert('something amazing is about to happen!');
+    });
+
     nokia.mh5.assetsPath = "http://api.maps.nokia.com/mobile/0.2.0/lib/";
     nokia.mh5.app.embed({
         domNode: "#app_location",
